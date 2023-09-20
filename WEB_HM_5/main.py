079f59365a7a7ccf6f7340b281b1ad741ff95ce8
igorgroni <igorgroni@gmail.com>
2023-09-20 20:15:11 +0300

    fixed the hm

--- WEB_HM_5/main.py
+++ WEB_HM_5/main.py
@@ -1,117 +1,63 @@
-import asyncio
 import argparse
-from currency_api import CurrencyAPI
-from currency_parser import CurrencyParser
+import asyncio
+import aiohttp
+import datetime
 
 
-async def main():
-    try:
-        parser = argparse.ArgumentParser(
-            description="Fetch currency rates for a specified number of days."
-        )
-        parser.add_argument(
-            "days",
-            type=int,
-            default=10,
-            nargs="?",
-            help="Number of days for currency rates (max 10)",
-        )
+class PrivatBankAPI:
+    BASE_URL = "https://api.privatbank.ua/p24api"
 
-        args = parser.parse_args()
-        days = args.days  # Number of days for currency rates
+    async def fetch_exchange_rates(self, date):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"{self.BASE_URL}/exchange_rates?json&date={date}"
+            ) as response:
+                data = await response.json()
+                return data
 
-        if days > 10:
-            print("Error: Number of days cannot exceed 10.")
-            return
 
-        api = CurrencyAPI()
-        data = await api.fetch_currency_data(days)
-        currency_rates = CurrencyParser.parse_currency_data(data)
+async def get_exchange_rates_for_dates(dates):
+    privat_bank_api = PrivatBankAPI()
+    results = []
 
-        print(f"Currency rates for EUR and USD for the last {days} days:")
-        for currency, rates in currency_rates.items():
-            print(f"{currency}, Sell - {rates['sell']}, Buy - {rates['buy']}")
+    for date in dates:
+        exchange_data = await privat_bank_api.fetch_exchange_rates(date)
+        formatted_data = {
+            date: {
+                "EUR": {
+                    "sale": exchange_data["exchangeRate"][0]["saleRateNB"],
+                    "purchase": exchange_data["exchangeRate"][0]["purchaseRateNB"],
+                },
+                "USD": {
+                    "sale": exchange_data["exchangeRate"][1]["saleRateNB"],
+                    "purchase": exchange_data["exchangeRate"][1]["purchaseRateNB"],
+                },
+            }
+        }
+        results.append(formatted_data)
 
-    except Exception as e:
-        print(f"Error: {e}")
-
-    # async def main(): Variant 2 (work??)
-    # try:
-    #     parser = argparse.ArgumentParser(description="Fetch currency rates for a specified number of days.")
-    #     parser.add_argument("days", type=int, default=10, nargs="?", help="Number of days for currency rates (max 10)")
-
-    #     args = parser.parse_args()
-    #     days = args.days  # Number of days for currency rates
-
-    #     if days > 10:
-    #         print("Error: Number of days cannot exceed 10.")
-    #         return
-
-    #     api = CurrencyAPI()
-    #     data = await api.fetch_currency_data(days)
-    #     currency_rates = CurrencyParser.parse_currency_data(data)
-
-    #     print(f"Currency rates for EUR and USD for the last {days} days:")
-    #     for currency, rates in currency_rates.items():
-    #         print(f"{currency}, Sell - {rates['sell']}, Buy - {rates['buy']}")
-
-    # except Exception as e:
-    #     print(f"Error: {e}")
+    return results
 
 
 if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <number_of_days>")
+        sys.exit(1)
+
+    try:
+        num_days = int(sys.argv[1])
+    except ValueError:
+        print("Invalid number of days")
+        sys.exit(1)
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    dates = [
+        (datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%d.%m.%Y")
+        for i in range(num_days)
+    ]
+    results = loop.run_until_complete(get_exchange_rates_for_dates(dates))
+    loop.close()
 
-
-# async def main():  Variant 3 (no work)
-#     try:
-#         parser = argparse.ArgumentParser(
-#             description="Fetch currency rates for a specified number of days."
-#         )
-#         parser.add_argument(
-#             "days",
-#             type=int,
-#             default=10,
-#             nargs="?",
-#             help="Number of days for currency rates (max 10)",
-#         )
-
-#         args = parser.parse_args()
-#         days = args.days
-
-#         if days > 10:
-#             print("Error: Number of days cannot exceed 10.")
-#             return
-
-#         api = CurrencyAPI()
-#         data = await api.fetch_currency_data(days)
-#         currency_rates = CurrencyParser.parse_currency_data(data)
-
-#         result = []
-
-#         for date, rates in currency_rates.items():
-#             entry = {date: {}}
-
-#             if "EUR" in rates:
-#                 entry[date]["EUR"] = {
-#                     "sale": rates["EUR"]["sell"],
-#                     "purchase": rates["EUR"]["buy"],
-#                 }
-
-#             if "USD" in rates:
-#                 entry[date]["USD"] = {
-#                     "sale": rates["USD"]["sell"],
-#                     "purchase": rates["USD"]["buy"],
-#                 }
-
-#             result.append(entry)
-
-#         print(result)
-
-#     except Exception as e:
-#         print(f"Error: {e}")
-
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
+    print(results)
