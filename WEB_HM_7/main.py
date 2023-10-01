ed737e0007ff70590681ea0d858f52597e7a4c11
igorgroni <igorgroni@gmail.com>
2023-10-01 19:52:45 +0300

    ++

---
+++ WEB_HM_7/main.py
@@ -0,0 +1,33 @@
+import my_select
+
+
+def main():
+    my_select.query_1()
+
+    # my_select.query_2()
+
+    # my_select.query_3()
+
+    # my_select.query_4()
+
+    # my_select.query_5()
+
+    # group_id = 3
+    # my_select.query_6(group_id)
+
+    # group_id = 2
+    # subject_id = 4
+    # my_select.query_7(group_id, subject_id)
+
+    # my_select.query_8()
+
+    # grade_id = 5
+    # my_select.query_9(grade_id)
+
+    # student_id = 15
+    # teacher_id = 1
+    # my_select.query_10(student_id, teacher_id)
+
+
+if __name__ == "__main__":
+    main()
