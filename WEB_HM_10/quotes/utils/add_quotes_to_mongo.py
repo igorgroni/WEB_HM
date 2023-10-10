b380d8cbf1333d21c0d6fad98ed491b2b985c67d
igorgroni <igorgroni@gmail.com>
2023-10-10 21:12:48 +0300

    updated method to connect to db

--- WEB_HM_10/quotes/utils/add_quotes_to_mongo.py
+++ WEB_HM_10/quotes/utils/add_quotes_to_mongo.py
@@ -1,9 +1,19 @@
 import json 
+import configparser
 
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-client = MongoClient("mongodb://localhost/")
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+mongo_user = config.get('DB', 'user')
+mongodb_pass = config.get('DB', 'pass')
+db_name = config.get('DB', 'db_name')
+domain = config.get('DB', 'domain')
+
+client = MongoClient(
+    f"mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority")
 
 db = client.hm10
 
