f0d6cd52d578c505cdfb51da3362c94e4f47bed0
igorgroni <igorgroni@gmail.com>
2023-10-12 13:15:24 +0300

    Added new Class

--- WEB_HM_10/quotes/quotesapp/models.py
+++ WEB_HM_10/quotes/quotesapp/models.py
@@ -4,18 +4,25 @@ from django.db import models
 
 
 class Author(models.Model):
+    fullname = models.CharField(max_length=25, null=False, unique=True)
+    born_date = models.CharField(max_length=50)
+    born_location = models.CharField(max_length=25, null=False, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
     name = models.CharField(max_length=25, null=False, unique=True)
 
-    def __str__(self):
-        return f"{self.name}"
-
-
 class Quote(models.Model):
-    # name = models.CharField(max_length=50, null=False)
-    description = models.CharField(max_length=150, null=False)
-    done = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-    authors = models.ManyToManyField(Author)
+    quote = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, default=None, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.name}"
+   
+
+
+
+
+  
